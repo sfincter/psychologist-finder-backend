df34491029148d0a7f79fb0c3b6d5ec8fa18318b
noName <samyrize77777@gmail.com>
2024-11-30 23:44:45 +0400

    fix cors

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*', // или указание конкретных доменов
+  methods: 'GET,POST,PUT,DELETE',
+}));
 app.use(express.json());
 
 // Подключение к MongoDB
